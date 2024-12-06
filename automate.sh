645c65badcc98b2b722d943a4340a91dec2dde0f
CAMILLE OUVRARD <camille@mbp-de-camille.home>
2024-12-06 23:55:02 +0100

    Updated automate.sh script

--- automate.sh
+++ automate.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Navigate to the project directory
+# Navigate to your project directory
 cd /Users/camille/MyPuppetProject4
 
 # Run the screenshot script
 /usr/local/bin/node screenshot.js
 
-# Commit and push the new screenshot to GitHub
+# Add the updated screenshot to Git
 git add latest.png
+
+# Commit the change with a timestamp
 git commit -m "Updated screenshot at $(date)"
+
+# Push the update to GitHub
 git push origin main
+
 
